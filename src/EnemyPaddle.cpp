1675df6aa7cfa06ce18a5bb1e963ba48bebd8a3d
josec1073 <josecarlos.travesi@gmail.com>
2021-06-04 20:09:39 +0200

    More improvements on IA. Now IA predictes ball position and has a range of error.

--- src/EnemyPaddle.cpp
+++ src/EnemyPaddle.cpp
@@ -2,44 +2,57 @@
 
 EnemyPaddle::EnemyPaddle(sf::Vector2f pos, float width, float height)
 	:
-	Paddle(pos, width, height)
+	Paddle(pos, width, height),
+	rnd(rd()),
+	dist(-50, 50)
 {
 	ballColllisionDir = -1.0f;
 }
 
+void EnemyPaddle::MoveTowards(float yDest)
+{
+	const float posYCenter = pos.y + height / 2.0f;
+	if (yDest < posYCenter)
+	{
+		dirY = -1.0f;
+	}
+	else if (yDest > posYCenter)
+	{
+		dirY = 1.0f;
+	}
+	// 50.0f is a magic number :(
+	if(yDest >= posYCenter - 50.0f && yDest <= posYCenter + 50.0f)
+	{
+		dirY = 0.0f;
+	}
+}
+
 void EnemyPaddle::Update(const float& dt, const Ball& ball, const sf::FloatRect& walls)
 {	
-	const float posYCenter = pos.y + height / 2.0f;
-	
 	timer += dt;
 	
 	if (timer >= movementDelay)
 	{
-		const float ballPosY = ball.GetPosition().y;
-		if (ballPosY > posYCenter)
-		{
-			dirY = 1.0f;
-		}
-		else
-		{
-			dirY = -1.0f;
-		}
+		const float nextBallPosY = CalculateYGoal(ball.GetPosition(), ball.GetDirection());
+		
+		MoveTowards(nextBallPosY);
 		timer = 0.0f;
-
-		if (ballPosY >= pos.y && ballPosY <= pos.y + height)
-		{
-			speed = 0.0f;
-		}
-		else
-		{
-			speed = 400.0f;
-		}
 	}
 	
 	
 	pos.y += dirY * speed * dt;
 	pos.y = ClampScreen(pos.y, walls);
 	paddle.setPosition(pos);
+}
+
+float EnemyPaddle::CalculateYGoal(const sf::Vector2f& ballPos, const sf::Vector2f& ballDir)
+{
+	// Calculates intersetion of the linear equation which ball follows, then adds a little error to more human behavior.
+	const float m = ballDir.y / ballDir.x;
+	const float n = -m * ballPos.x + ballPos.y;
+
+
+	return m * pos.x + n + (float)dist(rnd);
 }
 
 
--- src/EnemyPaddle.h
+++ src/EnemyPaddle.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Paddle.h"
+#include <random>
 
 class EnemyPaddle :
 	public Paddle
@@ -10,12 +11,19 @@ public:
 	EnemyPaddle(sf::Vector2f pos, float width, float height);
 
 	// Movement
+	void MoveTowards(float yDest);
 	virtual void Update(const float& dt, const Ball& ball, const sf::FloatRect& walls);
 
 private:
 	// Direction in Y axis of movement, used to follow the ball
-	static constexpr float movementDelay = 0.1f;
+	static constexpr float movementDelay = 0.25f;
 	float dirY = 1.0f;
 	float timer = 0.0f;
+	std::random_device rd;
+	std::mt19937 rnd;
+	std::uniform_real_distribution<> dist;
+
+	// Private methods
+	float CalculateYGoal(const sf::Vector2f& ballPos, const sf::Vector2f& ballDir);
 };
 
